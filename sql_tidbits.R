4e645b72bb07b95b0f38a20713e07ebcfdbd2dc1
Powell <Charles.Powell@ct.gov>
2021-02-03 18:17:51 -0500

    Updating the df_to_table() function likely tomorrow

--- sql_tidbits.R
+++ sql_tidbits.R
@@ -5,21 +5,23 @@ df_to_table <-
            append = TRUE) {
     
     ### error checking
+    if(overwrite == append)  stop("You can not append and overwrite at the same time")
     if(missing(df_name)) stop("You must specify a dataframe")
     if(!exists(deparse(substitute(df_name))))  stop("Can not find that dataframe")
     if(is.null(table_name)) stop("You must specify a table in the database")
     
     ### main code
     epi_connect <- DBI::dbConnect(odbc::odbc(), "epicenter")
-    # table_id <- DBI::Id(catalog = "DPH_COVID_IMPORT", 
-    #                     schema = "dbo", 
-    #                     table = table_name)
-    # DBI::dbWriteTable(epi_connect, 
-    #                   table_id, 
-    #                   df_name, 
-    #                   overwrite = overwrite, 
-    #                   append = append)
-    DBI::dbWriteTable(epi_connect, SQL("DPH_COVID_IMPORT.dbo.ODP_state_Result"), df_name, append = TRUE)
+    
+    # build the SQL string
+    fqdbn <- paste0("DPH_COVID_IMPORT.dbo.", table_name)
+    
+    DBI::dbWriteTable(epi_connect, 
+                      SQL(fqdbn), 
+                      df_name, 
+                      append = append, 
+                      overwrite = overwrite)
+    
     odbc::dbDisconnect(epi_connect)
   }
 
