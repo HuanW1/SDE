bfd385baa600ed5ba2381967b77af01b56f815c7
Powell <Charles.Powell@ct.gov>
2021-02-10 17:10:42 -0500

    tidying city cleaning

---
+++ helpers/address_helper.R
@@ -0,0 +1,14 @@
+data %>%
+  mutate(Patient_city = str_to_title(Patient_city),
+         Patient_county = str_to_title(Patient_county),
+         Patient_county = gsub(" County", "", .$Patient_county),
+         Bad_city = if_else(Patient_city %nin% county_table$CITY, TRUE, FALSE),
+         Bad_county = if_else(Patient_county %nin% county_table$COUNTY, TRUE, FALSE),
+         Bad_zipcode = if_else(Patient_zip_code %nin% zips$zipcode, TRUE, FALSE)) %>%
+  group_by(Bad_city, Bad_county, Bad_zipcode) %>%
+  summarise(n())
+
+
+  filter(Patient_city %nin% county_table$CITY |
+           Patient_county %nin% county_table$COUNTY) %>%
+  select(Patient_city, Patient_county)
