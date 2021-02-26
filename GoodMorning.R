3f35cd3eea6ca2068e6d568ba342ed5f31ea8421
Powell <Charles.Powell@ct.gov>
2021-02-26 09:47:47 -0500

    Adding script to check on what automated modules produce for a draft summary table.

---
+++ GoodMorning.R
@@ -0,0 +1,25 @@
+if(!dir.exists("L:/")) message("You need to have L drive mapped")
+
+.libPaths(c("L:/library", .libPaths()))
+
+library(tidyverse)
+library(lubridate)
+library(flextable)
+
+possibilities <-
+  list.files(path = "l:/",
+             pattern = "draft_table",
+             full.names = TRUE)
+
+which_one <-
+  as.data.frame(possibilities) %>%
+  cbind(as.data.frame(str_split(possibilities,
+                                pattern = "_|\\.",
+                                simplify = TRUE))) %>%
+  select(-V1, -V2, -V4, justdate = V3) %>%
+  mutate(justdate = as_datetime(justdate)) %>%
+  filter(justdate == max(justdate)) %>%
+  pull(possibilities)
+
+load(which_one)
+ovlsum
