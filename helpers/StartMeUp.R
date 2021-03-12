e4683a643b711fa3718ba18fb9762c1684f44f7c
Chuck <ibecav@gmail.com>
2021-03-12 16:09:46 -0500

    Trying to find the best way to level set libraries

---
+++ helpers/StartMeUp.R
@@ -0,0 +1,12 @@
+if(!dir.exists("L:/")) message("You need to have L drive mapped")
+
+DPH_packages <- c("rmarkdown", "kableExtra", "tidyverse", "lubridate", "sf",  
+                   "DBI", "odbc", "formatR", "knitr", "MMWRweek",
+                  "scales", "english", "flextable")
+
+sapply(DPH_packages, library, 
+       logical.return = TRUE, 
+       character.only = TRUE, 
+       warn.conflicts = FALSE, 
+       quietly = TRUE)
+
