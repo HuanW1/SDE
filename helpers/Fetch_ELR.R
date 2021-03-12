30bfdbc0c7642d96dcae6c115d8493112b0a6147
Powell <Charles.Powell@ct.gov>
2021-03-12 14:27:19 -0500

    Script to load correct libraries and pull the most current elr_linelist from L drive CGP Mar 12

---
+++ helpers/Fetch_ELR.R
@@ -0,0 +1,27 @@
+suppressPackageStartupMessages(library("tidyverse", lib.loc = "L:/newlib", quietly = TRUE, warn.conflicts = FALSE))
+library("lubridate", lib.loc = "L:/newlib", quietly = TRUE, warn.conflicts = FALSE)
+
+# step 1 get the most recenet elr_linelist fast and accurate
+
+
+# a. what are my choices
+possibilities <-
+  list.files(path = "l:/recent_rdata",
+             pattern = "elr_linelist_",
+             full.names = TRUE)
+
+# b. pick the newest
+which_one <-
+  as.data.frame(possibilities) %>%
+  cbind(as.data.frame(str_split(possibilities,
+                                pattern = "_|\\.",
+                                simplify = TRUE))) %>%
+  select(-V1, -V2, -V3, justdate = V4) %>%
+  mutate(justdate = as_datetime(justdate)) %>%
+  filter(justdate == max(justdate)) %>%
+  pull(possibilities)
+
+# c. load it with the name elr_linelist
+# takes a minute or so
+message("This will take about 2.5 minutes")
+load(which_one)
