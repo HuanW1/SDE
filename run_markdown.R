d9e7cead0150c889f5bbcc5f49b624ed51914fa2
Powell <Charles.Powell@ct.gov>
2021-03-14 20:00:00 -0400

    Updating sctipts to use StartMeUp

--- run_markdown.R
+++ run_markdown.R
@@ -1,6 +1,8 @@
 if(!dir.exists("L:/")) message("You need to have L drive mapped")
-.libPaths(c("L:/newlib", .libPaths()))
-library(knitr)
-library(rmarkdown)
+# .libPaths(c("L:/newlib", .libPaths()))
+# library(knitr)
+# library(rmarkdown)
+
+source("helpers/StartMeUp.R")
 
 rmarkdown::render("SQL_allgasnobrakes.rmd")
