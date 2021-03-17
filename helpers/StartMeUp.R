dfd9611f3062c3098ded2b7bcc054d302a0a3971
Powell <Charles.Powell@ct.gov>
2021-03-17 14:29:21 -0400

    Adding slider and stringdist to the load list CGP March 17th

--- helpers/StartMeUp.R
+++ helpers/StartMeUp.R
@@ -1,15 +1,16 @@
 if(!dir.exists("L:/")) message("You need to have L drive mapped")
 
-DPH_packages <- c("rmarkdown", "kableExtra", "tidyverse", "lubridate", "sf",  
+DPH_packages <- c("rmarkdown", "kableExtra", "tidyverse", "lubridate", "sf",
                    "DBI", "odbc", "formatR", "knitr", "MMWRweek",
-                  "scales", "english", "flextable")
+                  "scales", "english", "flextable", "slider",
+                  "stringdist")
 
-quiet_load <- function(x) {  
+quiet_load <- function(x) {
   suppressPackageStartupMessages(library(x,
                                          lib.loc = "l:/newlib/",
-                                         logical.return = TRUE, 
-                                         character.only = TRUE, 
-                                         warn.conflicts = FALSE, 
+                                         logical.return = TRUE,
+                                         character.only = TRUE,
+                                         warn.conflicts = FALSE,
                                          quietly = TRUE,
                                          attach.required = TRUE))
 }
