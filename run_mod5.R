bdea5dbe57cae7e338147ff482afdee671e551c9
Senetcky <Alexander.Senetcky@ct.gov>
2021-04-22 16:31:42 -0400

    Delete: deleted unneeded script
    incidence stuff was combined with all the other thursday outputs and run_mod5 was changed accordingly

--- run_mod5.R
+++ run_mod5.R
@@ -1,13 +1,11 @@
 #run module 5
 source("helpers/StartMeUp.R")
 thursday <- lubridate::wday(lubridate::today()) == 5
-
 source("module_5/mod5_setup.R")
-#source("module_5/gary_odp_outputs.R")
-#source("module_5/misc_requested_outputs.R") #right now it's only county7days.csv which isn't being run atm.  The mastereventid has been moved to thursday now
+source("module_5/gary_odp_outputs.R")
+#source("module_5/misc_requested_outputs.R")
 if(thursday){
-  #source("module_5/incidence_town_alerts.R")
-  #source("module_5/misc_thursday_requested_outputs.R")
+  source("module_5/misc_thursday_requested_outputs.R")
 }
-# rm(list = ls())
-# gc(verbose = FALSE)
+rm(list = ls())
+gc(verbose = FALSE)
