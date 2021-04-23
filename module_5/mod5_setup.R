49e26cd9ec479033af528a8015cd638324369344
Senetcky <Alexander.Senetcky@ct.gov>
2021-04-23 07:36:10 -0400

    #feature: cleanup
    commenting on code and slotting in chuck's sql/df functions where I can

--- module_5/mod5_setup.R
+++ module_5/mod5_setup.R
@@ -5,7 +5,6 @@ message("Module 5 Setup process will now begin")
 message("Common files used across all outputs will be loaded, be sure to clear up your RAM")
 
 ###0 libraries, connections and data, oh my ####
-gary_con <- DBI::dbConnect(odbc::odbc(), "epicenter")
 message("CSV and SQL writes are OFF")
 csv_write <- FALSE
 SQL_write <- FALSE
@@ -17,16 +16,16 @@ source("helpers/Fetch_ELR.R")
 source("helpers/Fetch_cha.R")
 
 ####1 Load Lookups and Dependancies ####
+#vectors
 age_labels <- c("0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", ">=80")
-
 gary_age_labels <- c("cases_age0_9", "cases_age10_19", "cases_age20_29", "cases_age30_39", "cases_age40_49", "cases_age50_59", "cases_age60_69", "cases_age70_79", "cases_age80_Older" )
-
-statement <- paste0("SELECT * FROM [DPH_COVID_IMPORT].[dbo].[RPT_TOWN_CODES]")
-city_file <-  DBI::dbGetQuery(conn = gary_con , statement = statement)
-
-HospitalizedCases <- tibble(HospitalizedCases = cha_c$today[9])
-dec <- case %>% filter(outcome == "Died") %>%  nrow()
 counties <-c("Fairfield County", "Hartford County", "Litchfield County", "Middlesex County", "New Haven County", "New London County", "Tolland County", "Windham County")
+
+#city lookup
+city_file <- tibble(table_to_df("RPT_TOWN_CODES"))
+
+#commonly used cha vector
+HospitalizedCases <- tibble(HospitalizedCases = cha_c$today[9])
 
 #sets up rea tables and percents
 source("race_ethnicity/race_ethnicity_setup.R")
@@ -36,16 +35,20 @@ ct_rea_denoms <- county_rea_denoms %>%
   group_by(hisp_race) %>% 
   summarize(pop = sum(pop))
 
+#date used for graphs and DateUpdated type of variables
 graphdate <- Sys.Date() - 1
 
-#only keep what we need
+#only keep what we need from cases and elr
 case <- case %>% 
-  select(c(bigID,eventid,disease_status,age,gender,street, city,county, state,zip_code, hisp_race,outcome, spec_col_date,date, age_group,death_date, cong_yn)) %>% 
+  select(c(bigID,eventid,disease_status,age,gender,street, city,county, state,zip_code, hisp_race,outcome, spec_col_date,date, 
+           age_group,death_date, cong_yn)) %>% 
   mutate(outcome = ifelse(is.na(outcome), "Survived", outcome)) 
 
 elr_linelist <- elr_linelist %>% 
   select(c(eventid, bigID, city,county, test_method, result,spec_col_date, pcrag, spec_rec_date)) %>% 
   mutate(eventid = as.numeric(eventid))
 
-odbc::dbDisconnect(gary_con)
+#quick easy death count used throughout
+dec <- case %>% filter(outcome == "Died") %>%  nrow()
+
 message("Module 5 setup is now complete")
